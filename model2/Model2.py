d543e24dd55e644da11d98535c6c9a6cf70ebaa9
yuki <gandamexsia@yahoo.co.jp>
2020-01-09 11:02:26 +0900

    update

---
+++ model2/Model2.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from keras.layers import Input, Dense, Embedding, LSTM, Dropout, concatenate
+from keras.models import Model
+from keras.callbacks import EarlyStopping
+from keras.utils import plot_model
+import numpy as np
+from keras.optimizers import Adam
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+import keras.backend as K
+from functools import partial
+import matplotlib.pyplot as plt
+
+
+# +
+def normalize_y_pred(y_pred):
+    return K.one_hot(K.argmax(y_pred), y_pred.shape[-1])
+
+def class_true_positive(class_label, y_true, y_pred):
+    y_pred = normalize_y_pred(y_pred)
+    return K.cast(K.equal(y_true[:, class_label] + y_pred[:, class_label], 2), K.floatx())
+
+def class_accuracy(class_label, y_true, y_pred):
+    y_pred = normalize_y_pred(y_pred)
+    return K.cast(K.equal(y_true[:, class_label], y_pred[:, class_label]),
+                  K.floatx())
+
+def class_precision(class_label, y_true, y_pred):
+    y_pred = normalize_y_pred(y_pred)
+    return K.sum(class_true_positive(class_label, y_true, y_pred)) / (K.sum(y_pred[:, class_label]) + K.epsilon())
+
+
+def class_recall(class_label, y_true, y_pred):
+    return K.sum(class_true_positive(class_label, y_true, y_pred)) / (K.sum(y_true[:, class_label]) + K.epsilon())
+
+
+def class_f_measure(class_label, y_true, y_pred):
+    precision = class_precision(class_label, y_true, y_pred)
+    recall = class_recall(class_label, y_true, y_pred)
+    return (2 * precision * recall) / (precision + recall + K.epsilon())
+
+
+def true_positive(y_true, y_pred):
+    y_pred = normalize_y_pred(y_pred)
+    return K.cast(K.equal(y_true + y_pred, 2),
+                  K.floatx())
+
+
+def micro_precision(y_true, y_pred):
+    y_pred = normalize_y_pred(y_pred)
+    return K.sum(true_positive(y_true, y_pred)) / (K.sum(y_pred) + K.epsilon())
+
+
+def micro_recall(y_true, y_pred):
+    return K.sum(true_positive(y_true, y_pred)) / (K.sum(y_true) + K.epsilon())
+
+
+def micro_f_measure(y_true, y_pred):
+    precision = micro_precision(y_true, y_pred)
+    recall = micro_recall(y_true, y_pred)
+    return (2 * precision * recall) / (precision + recall + K.epsilon())
+
+
+def average_accuracy(y_true, y_pred):
+    class_count = y_pred.shape[-1]
+    class_acc_list = [class_accuracy(i, y_true, y_pred) for i in range(class_count)]
+    class_acc_matrix = K.concatenate(class_acc_list, axis=0)
+    return K.mean(class_acc_matrix, axis=0)
+
+
+def macro_precision(y_true, y_pred):
+    class_count = y_pred.shape[-1]
+    return K.sum([class_precision(i, y_true, y_pred) for i in range(class_count)]) / K.cast(class_count, K.floatx())
+
+
+def macro_recall(y_true, y_pred):
+    class_count = y_pred.shape[-1]
+    return K.sum([class_recall(i, y_true, y_pred) for i in range(class_count)]) / K.cast(class_count, K.floatx())
+
+
+def macro_f_measure(y_true, y_pred):
+    precision = macro_precision(y_true, y_pred)
+    recall = macro_recall(y_true, y_pred)
+    return (2 * precision * recall) / (precision + recall + K.epsilon())
+
+def weight_variable(shape):
+    return K.truncated_normal(shape, stddev = 0.01)
+
+
+# -
+
+#データの読み込み
+use_data = pd.read_csv(filepath_or_buffer="Datas/pickup_data.csv", encoding="utf_8", sep=",")
+print(len(use_data))
+use_data.info()
+
+#userID
+test = open("user.txt","r", encoding="utf-8")
+uID = test.readlines()
+test.close()
+print(len(uID))
+
+id_list = []
+for i in uID:
+    if i in id_list:
+        continue
+    else:
+        id_list.append(i)
+
+print(len(id_list))
+
+post_user = []
+for i in uID:
+    for j in range(len(id_list)):
+        if i == id_list[j]:
+            post_user.append(j)
+
+n_postUser = np.array(post_user)
+
+# +
+maxlen = 50
+train = 0.7
+validation = 0.1
+max_words = 20000
+
+# word indexを作成
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+
+word_index = tokenizer.word_index
+print("Found {} unique tokens.".format(len(word_index)))
+
+data = pad_sequences(sequences, maxlen=maxlen)
+
+# バイナリの行列に変換
+categorical_labels = to_categorical(label)
+labels = np.asarray(categorical_labels)
+
+print("Shape of data tensor:{}".format(data.shape))
+print("Shape of label tensor:{}".format(labels.shape))
+
+# 行列をランダムにシャッフルする
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+users = n_postUser[indices]
+
+indices = [int(len(labels) * n) for n in [train, train + validation]]
+x1_train, x1_val, x1_test = np.split(data, indices)
+x2_train, x2_val, x2_test = np.split(users, indices)
+y_train, y_val, y_test = np.split(labels, indices)
+
+# +
+p_input = Input(shape=(50, ), dtype='int32', name='Input_postText')
+i_input = Input(shape=(1, ), name='Input_ids')
+
+#テキストの学習
+em = Embedding(input_dim=20000, output_dim=50, input_length=50, name='Embedding')(p_input)
+d_em = Dropout(0.5)(em)
+lstm_out = LSTM(32, name='LSTM')(d_em)
+d_lstm_out = Dropout(0.5)(lstm_out)
+#2つ目のデータ学習
+i2 = Dense(16, activation='relu', name = 'dence1')(i_input)
+d_i2 = Dropout(0.5)(i2)
+x = concatenate([d_lstm_out, d_i2], name='merge1')
+
+m2 = Dense(16, activation='relu', name = 'dence')(x)
+d_m2 = Dropout(0.5)(m2)
+output = Dense(2, activation='softmax', name = 'output')(d_m2)
+
+optimizer = Adam(lr=1e-3)
+model = Model(inputs=[p_input, i_input], outputs = output)
+model.compile(optimizer=optimizer, loss='categorical_crossentropy',  metrics=['acc', macro_precision, macro_recall, macro_f_measure])
+model.summary()
+#plot_model(model, show_shapes=True, show_layer_names=True, to_file='model_image/model2.png')
+
+
+early_stopping = EarlyStopping(patience=2, verbose=1)
+# -
+
+history = model.fit([x1_train, x2_train], y_train,
+                    epochs=100, 
+                    batch_size=256,
+                    validation_data=([x1_val, x2_val], y_val))
+
+loss_and_metrics = model.evaluate([x1_test, x2_test], y_test)
+print(loss_and_metrics)
+
+classes = model.predict([x1_test, x2_test])
+np.savetxt('Mult_predict.csv', classes, delimiter = ',')
+
+model.metrics_names
+
+# +
+# %matplotlib inline
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'b--', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+#plt.savefig("RNN_tweet/Figs/N_method1/N_acc.png")
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b--', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+#plt.savefig("RNN_tweet/Figs/N_method1/N_loss.png")
+
+plt.figure()
+
+# +
+fig = plt.figure()
+ax_acc = fig.add_subplot(111)
+ax_acc.plot(epochs, val_acc, 'b--', label='Validation acc')
+plt.legend(bbox_to_anchor=(0, 1), loc='upper left', borderaxespad=0.5, fontsize=10)
+
+ax_loss = ax_acc.twinx()
+ax_loss.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.legend(bbox_to_anchor=(0, 0.9), loc='upper left', borderaxespad=0.5, fontsize=10)
+plt.title('Validation acc and Validation loss')
+ax_acc.set_xlabel('epochs')
+ax_acc.set_ylabel('Validation acc')
+ax_loss.grid(True)
+ax_loss.set_ylabel('Validation loss')
+#plt.savefig("N_f_measure.png")
+plt.show()
+# -
+
+
+
